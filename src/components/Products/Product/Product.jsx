5319490954f5e8e80b6431420b1594fdd2bc385e
Jasmin Harbinja <57997257+JasminHarbinja@users.noreply.github.com>
2022-03-10 12:44:09 +0100

    Add files via upload

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
 
-export const Product = () => {
+import useStyles from './styles';
+
+export const Product = ({ product }) => {
+    const classes = useStyles();
   return (
-    <div>
+    <Card className={classes.root}>
+        <CardMedia className={classes.media} image='' title={Product.name}/>
+        <CardContent>
+            <div className={classes.cardContent}>
+                <Typography variant='h5' gutterBottom>
+                    {product.name}
+                </Typography>
+                <Typography variant='h5' >
+                    {product.prace}
+                </Typography>
+            </div>
+            <Typography variant='h2' color="textSecondery">
+                    {product.description}
+                </Typography>
+        </CardContent>
+        <CardActions disableSpacing className={classes.cardActions}>
+            <IconBotton aria-label="Add to Cart">
+                <AddShoppingCart />
+            </IconBotton>
+        </CardActions>
         
-    </div>
+
+    </Card>
   )
 }
-export default Product
+export default Product;
